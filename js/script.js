242ed56a7af091a458de1b1ce98f25c08d3bc4d6
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-05-31 14:05:25 -0400

    Added a loop to print a list of students and their course

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("test");
+var students = ["John", "FSF", "Jamil", "FSF", "Roman", "FSF"];
+
+
+
+
+for (var index = 0; index < students.length; index += 2) {
+    console.log("Name:", students[index]);
+    console.log("Course:", students[index + 1]);
+}
+
+// This is the same thing:
+// for (var index = 0; index < students.length; index += 2) {
+//     console.log("Name:", students[index]);
+//     console.log("Course:", students[index + 1]);
+
+//     index += 1;
+// }
