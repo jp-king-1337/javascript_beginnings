dc5d46a005b3a635fad192a821d0e890ea74fbcc
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-05-31 14:13:13 -0400

    learned about functions

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 var students = ["John", "FSF", "Jamil", "FSF", "Roman", "FSF"];
 
 
-
-
-for (var index = 0; index < students.length; index += 2) {
-    console.log("Name:", students[index]);
-    console.log("Course:", students[index + 1]);
-}
+// for (var index = 0; index < students.length; index += 2) {
+//     console.log("Name:", students[index]);
+//     console.log("Course:", students[index + 1]);
+// }
 
 // This is the same thing:
 // for (var index = 0; index < students.length; index += 2) {
@@ -15,3 +13,13 @@ for (var index = 0; index < students.length; index += 2) {
 
 //     index += 1;
 // }
+
+
+function printStudentInfo() {
+    console.log("Welcome to the class!");
+    console.log("Name:", students[0]);
+    console.log("Course:", students[1]);
+    console.log("Please utilize office hours for help!");
+}
+
+printStudentInfo ()
