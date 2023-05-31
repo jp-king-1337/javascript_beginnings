ad03b71b17460d638807860092ec691a47e330a6
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-05-31 14:53:46 -0400

    Called a function inside of a loop

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 var students = ["John", "FSF", "Jamil", "FSF", "Roman", "FSF"];
 
 
+// TASK: Use the for loop to call the function and print the full list of students.
+
+for (var index = 0; index < students.length; index += 2) {
+    printStudentInfo(students[index], students[index + 1]);
+}
+
+function printStudentInfo(studentName, studentCourse) {
+console.log("Welcome to the class!");
+console.log("Name:", studentName);
+console.log("Course:", studentCourse);
+console.log("Please utilize office hours for help!");
+}
+
+
+
 // for (var index = 0; index < students.length; index += 2) {
 //     console.log("Name:", students[index]);
 //     console.log("Course:", students[index + 1]);
@@ -15,19 +30,26 @@ var students = ["John", "FSF", "Jamil", "FSF", "Roman", "FSF"];
 // }
 
 
-function printStudentInfo(studentName, studentCourse) {
-    // The content in () in that function is a parameter.
-    console.log("Welcome to the class!");
-    console.log("Name:", studentName);
-    console.log("Course:", studentCourse);
-    console.log("Please utilize office hours for help!");
-}
+// function printStudentInfo(studentName, studentCourse) {
+//     // The content in () in that function is a parameter.
+//     console.log("Welcome to the class!");
+//     console.log("Name:", studentName);
+//     console.log("Course:", studentCourse);
+//     console.log("Please utilize office hours for help!");
+// }
 
 // printStudentInfo(0);
 // The 0 is an argument.
-printStudentInfo(students[0], students[1]);
+// printStudentInfo(students[2], students[3]);
 
 
 // printStudentInfo(students[0]);
 // Equivalent to:
 // printStudentInfo("John");
+
+
+// function printStuff(something, name, age) {
+//     console.log(something);
+// }
+
+// printStuff("JD", 43, "something");
