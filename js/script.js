2632e9c2f6a3d89dfba13a46be091d0acffb63e3
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-01 12:08:15 -0400

    added a new variable for combined, which concatenates all of the arrays into one, and then replaced lowercase in the for loop with combined

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialCharacters = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+var combined = lowercase.concat(uppercase).concat(numbers).concat(specialCharacters);
 
 // Prints lowercase followed by uppercase followed by numbers
 // var combined = lowercase.concat(uppercase).concat(numbers);
@@ -36,8 +37,9 @@ var password = "";
 
 for (var index = 0; index < amount; index++) {
     var random = Math.random();
-    var ranIndex = Math.floor(random * lowercase.length);
-    password += lowercase[ranIndex];
+    var ranIndex = Math.floor(random * combined.length);
+    password += combined[ranIndex];
 }
 
 console.log(password);
+
