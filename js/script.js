cac9a045fe32e4652ab7d65eccbc7f10cdf5699a
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-02 14:04:40 -0400

    more object learning

--- js/script.js
+++ js/script.js
@@ -3,14 +3,35 @@
 var oldStudentData = ["Matthew", "FSF"];
 var studentData = {
     name: "Matthew",
-    course: "FSF"
+    course: "FSF",
+    university: "Rutgers",
+    location: "New Jersey",
+    age: 26,
+    someFunc: function() {
+        console.log("Wait, what?!");
+    }
 };
+
+
+studentData.someFunc();
 
 // var studentName = students[0];
 
 // var studentCourse = students[1];
 
-var studentName = studentData.name;
-var studentCourse = studentData.course;
+// var studentName = studentData.name;
+// var studentCourse = studentData.course;
 
-console.log(studentName, studentCourse);
+// console.log(studentName, studentCourse);
+
+// console.log(studentName.location);
+
+// var fruits = ["orange", "apple", "grape"];
+// var str = "some string";
+
+// console.log(str.length);
+
+// var strData = {
+//     value: "some string",
+//     length: 11
+// }
