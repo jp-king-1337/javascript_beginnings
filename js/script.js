5371e594e83e22571134d80ebe566b0119b2b8b0
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-02 13:45:22 -0400

    learning about objects

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+var oldStudentData = ["Matthew", "FSF"];
+var studentData = {
+    name: "Matthew",
+    course: "FSF"
+};
+
+var studentName = students[0];
+
+var studentCourse = students[1];
+
+console.log(studentName, studentCourse);
