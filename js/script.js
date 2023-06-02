73df159e4b685800e27029b233fc11b3614d3979
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-02 10:29:33 -0400

    removed things - starting from scratch on the generatePass function today

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// ChatGPT generated this with the prompt: can you provide 4 js arrays - lowercase letters, uppercase, numbers, and special characters? 
-// var lowercaseLetters = Array.from({length: 26}, (_, index) => String.fromCharCode(97 + index));
-// // Output: ['a', 'b', 'c', ..., 'x', 'y', 'z']
-
-// var uppercaseLetters = Array.from({length: 26}, (_, index) => String.fromCharCode(65 + index));
-// // Output: ['A', 'B', 'C', ..., 'X', 'Y', 'Z']
-
-// var numbers = Array.from({length: 10}, (_, index) => String.fromCharCode(48 + index));
-// // Output: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-// var specialCharacters = ['!', '@', '#', '$', '%', '&', '*'];
-// // Output: ['!', '@', '#', '$', '%', '&', '*']
-
-
-
 // ChatGPT generated this with the adjusted prompt: can you provide 4 js arrays - lowercase letters, uppercase, numbers, and special characters? I don't need them as variables, just the array content itself
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -27,15 +12,16 @@ var combinedArrays = lowercase.concat(uppercase).concat(numbers).concat(specialC
 // var includeLowercase = confirm("Click OK if you want lowercase letters.");
 // var includeUppercase = confirm("Click OK if you want uppercase letters.");
 
+
 // TASK: create a function that gets the amount of chars and returns a list of the amount and booleans for each of the character sets (charsets).
 function getUserInput() {
     var amount = Number(prompt("Please provide an amount."));
     var includeLowercase = confirm("Click OK if you want to include lowercase letters.");
     var includeUppercase = confirm("Click OK if you want to include uppercase letters.");
     var includeNumbers = confirm("Click OK if you want to include numbers.");
-    var includeSpecialCharacters = confirm("Click OK if you want to include special characters.");
+    var includeSpecials = confirm("Click OK if you want to include special characters.");
 
-    var output = [amount, includeLowercase, includeUppercase, includeNumbers, includeSpecialCharacters];
+    var output = [amount, includeLowercase, includeUppercase, includeNumbers, includeSpecials];
     return output;
 }
 
@@ -55,7 +41,7 @@ function generatePass() {
     var includeLowercase = choices[1];
     var includeUppercase = choices[2];
     var includeNumbers = choices[3];
-    var includeSpecialCharacters = choices[4];
+    var includeSpecials = choices[4];
 
     // Conditionally check each boolean to determine if we add to the array.
     if (includeLowercase) {
@@ -70,7 +56,7 @@ function generatePass() {
         combinedChars = combinedChars.concat(numbers);
     }
 
-    if (includeSpecialCharacters) {
+    if (includeSpecials) {
         combinedChars = combinedChars.concat(specialCharacters);
     }
 
@@ -85,15 +71,3 @@ function generatePass() {
 
 var pass = generatePass();
 console.log(pass);
-
-
-
-
-
-// function sum(a, b) {
-//     return a + b;
-// }
-
-// var result = sum(5, 10);
-
-// console.log(result);
