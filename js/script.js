85ef753de1fcf5519ed8351e023f378712186ad2
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-05 12:29:50 -0400

    time for lunch break

--- js/script.js
+++ js/script.js
@@ -8,15 +8,77 @@ var users = [
     },
     {
         name: "Jane",
-        age: 35
+        age: 35,
+        hobbies: ["tennis", "hiking"]
     },
     {
         name: "Bob",
+        healthcare: {
+            locations: {
+                pharmacy: "555 Cool St.",
+                emergency: "555 Help Dr.",
+            },
+            medications: {
+                amount: 50,
+                types: [
+                    {
+                        bloodPressure: "Amlodapine",
+                        pillSize: "10mg"
+                    },
+                    {
+                        painRelief: "Tylenol",
+                        pillSize: "500mg"
+                    }
+                ]
+            }
+        },
         age: 99
     },
 ]
 
-console.log(users[1]);
+// console.log(users[2].healthcare.medications.types[1].pillSize);
+// console.log(users[1].name)
+// users[1] is the object - followed by a . - followed by a key
+// console.log(users[1].hobbies[1]);
+// goes deeper - you can dig as deep in as necessary with this method
+// console.log(users[2].healthcare.medications.types.bloodPressure);
+// it can go all the way down as long as you reference the right object and key
+
+
+var jd = {
+    name: "JD",
+    age: 43,
+    hobbies: ["pickleball", "fishing"],
+    haveBirthday: function () {
+        this.age++;
+        // console.log(this.age);
+        // this doesn't point to the variable that = this object, it points directly to the object itself, allowing you to change the variable name
+    },
+    printSomething: function (txt) {
+        console.log(txt);
+    },
+    printAge: function () {
+        console.log(this.age);
+    },
+    printHobbiesByIndex: function(index) {
+        console.log(this.hobbies[index]);
+        // starts at this (the parent object), references the hobbies property, then grabs an item by the index number specified when we call the function
+    }
+}
+
+var un = {
+    something: ["apple", "orange"]
+};
+
+console.log(un.blah);
+
+
+// jd.haveBirthday();
+// we have to call the object js and then the function
+
+
+
+
 
 
 
@@ -39,4 +101,4 @@ var studentData = {
 };
 
 
-studentData.printInfo();
+// studentData.printInfo();
