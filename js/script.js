3d1a7d7539779b77c42ea70251ef2f389a2ce7dc
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-08 12:06:26 -0400

    Learned to prevent the refresh that is a default part of the submission on a form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+var formEl = document.querySelector("#user-form");
 var input = document.querySelector("#name-input");
-// var h3 = document.querySelector("h3");
 
 function getUserInput(eventObj) {
     // eventObj.preventDefault();
-    // h3.innerText = input.value
-    // console.log(input.value);
     if (eventObj.keyCode === 13) {
         console.log("enter pressed");
     }
 }
 
 input.addEventListener("keydown", getUserInput);
+
+formEl.addEventListener("submit", function (eventObj) {
+    eventObj.preventDefault();
+});
