b7479e812a796633fd64c6b623b5fc49cfbeefa3
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-08 12:01:55 -0400

    learning about eventListeners for keyboard

--- js/script.js
+++ js/script.js
@@ -1,40 +1,13 @@
-// Target all the li's in the ul and add an event listener to each li that console logs the innerText of the li
+var input = document.querySelector("#name-input");
+// var h3 = document.querySelector("h3");
 
-// Target the ul and add an event listener that console logs the innerText of the li tha was clicked
-
-var listItem = document.querySelectorAll("ul li");
-var unordList = document.querySelector("ul");
-var userAnswer = "Document Object Model"
-
-// listItem.forEach(function(li) {
-//     li.addEventListener("click", function() {
-//         console.log(li.innerText);
-//     })
-// });
-
-// MY SOLUTION
-// unordList.addEventListener("click", function (ul) {
-//     if (ul.target.tagName === "LI") {
-//         console.log(ul.target.innerText);
-//     }
-// });
-
-// JD SOLUTION
-function clicked(eventObj) {
-    var el = eventObj.target;
-
-    if (el.tagName === "LI") {
-        if (el.innerText === userAnswer) {
-            console.log("You are correct!");
-            el.classList.add("correct");
-            alert("You are correct!");
-            if (el.innerText === !userAnswer) {
-                el.classList.add("wrong");
-            }
-        } else {
-            console.log("Wrong...");
-        }
+function getUserInput(eventObj) {
+    // eventObj.preventDefault();
+    // h3.innerText = input.value
+    // console.log(input.value);
+    if (eventObj.keyCode === 13) {
+        console.log("enter pressed");
     }
 }
 
-unordList.addEventListener("click", clicked)
+input.addEventListener("keydown", getUserInput);
