3b1f640aeb3ac72c8745762cb01f35b43c6c5cbe
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-07 20:18:32 -0400

    Completed the activity JD gave us

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-// Target all the li's in the ul and add an event
-// listener to each li that console logs the innerText
-// of the li
+// Target all the li's in the ul and add an event listener to each li that console logs the innerText of the li
 
-// Target the ul and add an event listener that console
-// logs the innerText of the li tha was clicked
+// Target the ul and add an event listener that console logs the innerText of the li tha was clicked
+
+var listItem = document.querySelectorAll("ul li");
+var unordList = document.querySelector("ul");
+
+listItem.forEach(function(li) {
+    li.addEventListener("click", function() {
+        console.log(li.innerText);
+    })
+});
+
+unordList.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+        console.log(event.target.innerText);
+    }
+});
