b015c992a9c0a798aeb8a6ed7235b21b60f21bbf
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-08 14:23:27 -0400

    making a click counter git add -A git statusda
    asdadfa

--- js/script.js
+++ js/script.js
@@ -1,70 +1,13 @@
-var formEl = document.querySelector("#user-form");
-var emailInput = document.querySelector("#email-input");
-var passInput = document.querySelector("#pass-input");
-var emailOutput = document.querySelector("#user-email");
-var passOutput = document.querySelector("#user-pass");
+var countOutput = document.querySelector("#count-output");
+var clickBtn = document.querySelector("#click-button");
+var clicks = 0;
+
+// TASK: Create a function that increases clicks each time the button is pressed and outputs the click amount to the window.
+function addClick() {
+    console.log("clicked");
 
 
-// Retrieve the user data from localStorage
-function getUserData() {
-    var data = {}
-
-    data.email = localStorage.getItem("email-address");
-    data.password = localStorage.getItem("password-value");
-
-    return data;
 }
 
 
-// Outputs the user info to the window
-function showUserData() {
-    var userData = getUserData();
-
-    emailOutput.innerText = `Email: ${userData.email}`;
-    passOutput.innerText = `Password: ${userData.password}`;
-}
-
-
-function getUserInput(eventObj) {
-    eventObj.preventDefault();
-
-    var email = emailInput.value;
-    var pass = passInput.value;
-
-    localStorage.setItem("email-address", email);
-    // email-address is the key, email is the value
-    localStorage.setItem("password-value", pass);
-
-    showUserData();
-}
-
-// Initial process or app start tasks
-formEl.addEventListener("submit", getUserInput); showUserData();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var emailInput = document.querySelector("#email-input");
-
-// function getUserInput(eventObj) {
-//     // eventObj.preventDefault();
-//     if (eventObj.keyCode === 13) {
-//         console.log("enter pressed");
-//     }
-// }
-
-// emailInput.addEventListener("keydown", getUserInput);
-
-// formEl.addEventListener("submit", function (eventObj) {
-//     eventObj.preventDefault();
-// });
+clickBtn.addEventListener("click", addClick);
