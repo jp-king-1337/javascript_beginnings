715a72fd8d38045525ef21dedbcafe5b1c4564fd
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-09 12:06:01 -0400

    learning more about adding elements into HTML with JS

--- js/script.js
+++ js/script.js
@@ -3,17 +3,6 @@ var clickBtn = document.querySelector("#click-button");
 var input = document.querySelector("#name-input")
 var clicks = 0;
 
-var users = localStorage.getItem("users");
-var jsVal = JSON.parse(users);
-console.log(jsVal)
-
-
-// BASIC:
-// Store the user name and click amount to localStorage
-// ADVANCED:
-// Store an array of objects. Each object will represent the user that entered their name and the amount of clicks they performed
-// When the user enters their name, you’ll grab the array, add a user object to it and then replace the old array with the new array
-
 function addClick() {
     clicks++;
 
@@ -30,9 +19,15 @@ function clearData() {
 
 function getUserData() {
     var rawData = localStorage.getItem("users");
-    var parsed = JSON.parse(rawData);
+    var parsed = JSON.parse(rawData) || [];
 
     return parsed;
+}
+
+function saveUserData(arr) {
+    var jsonVal = JSON.stringify(arr);
+
+    localStorage.setItem("users", jsonVal);
 }
 
 function saveUserClicks(eventObj) {
@@ -42,15 +37,50 @@ function saveUserClicks(eventObj) {
             clicks: clicks
         };
 
-        var jsonVersion = JSON.stringify([userScore]);
-        localStorage.setItem("users", [jsonVersion]);
+        var usersArray = getUserData();
+        usersArray.push(userScore);
+
+        saveUserData(usersArray);
+        showScores();
 
         clearData();
     }
 
 }
 
+function showScores() {
+    var users = getUserData();
+    var scoresDiv = document.querySelector(".scores");
+    var div = document.createElement("div");
+
+    scoresDiv.innerHTML = "";
+
+    if (users.length === 0) {
+        // div.classList.add("score");
+
+        // var p = document.createElement("p");
+        // p.innerText = `No ${userObj.name}s or ${userObj.clicks} Yet!`
+        // div.append(p);
+
+        scoresDiv.innerHTML = "";
+    }
+
+    for (var userObj of users) {
+        div.classList.add("score");
+
+        var h3 = document.createElement("h3");
+        h3.innerText = `Name: ${userObj.name}`;
+        div.append(h3);
+
+        var p = document.createElement("p");
+        p.innerText = `Clicks: ${userObj.clicks}`;
+        div.append(p)
+
+        scoresDiv.append(div);
+    }
+}
+
 clickBtn.addEventListener("click", addClick);
 input.addEventListener("keydown", saveUserClicks);
 
-
+// showScores();
